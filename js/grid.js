e6a04fd566e2aad5478b5412e3eb22a424a2f30b
Natalia Agarkowa <agarkowa@gmail.com>
2018-07-03 11:53:26 +0200

    add orientation

--- js/grid.js
+++ js/grid.js
@@ -1,20 +1,36 @@
-window.onresize = function makeResponsive(event){
-  console.log('Oops');
-  let orientation = window.screen.orientation,
+window.addEventListener('resize', function makeResponsive(e));
+
+let makeResponsive = function(event){
+  console.log('OOooopps');
+  let orientation = screen.msOrientation || (screen.orientation || screen.mozOrientation || {}).type;
   widthWindow = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     console.log(widthWindow, orientation);
-    if (widthWindow >= 1200){
-    console.log("========= Extra Large===============");
-    }  else if(widthWindow >= 993 && widthWindow < 1199){
-      console.log('============Large===================');
-    }  else if (widthWindow >= 768 && widthWindow < 992){
-      console.log('============Medium================');
-    } else if (widthWindow >= 576 && widthWindow < 767){
-      console.log("==============Small===============");
-    } else if (widthWindow < 576){
-      console.log('=============X-Small=============');
+    if(orientation === "landscape-primary"){
+      if (widthWindow >= 1200){
+        console.log("========= Extra Large landscape-primary===============");
+      }  else if(widthWindow >= 993 && widthWindow < 1199){
+        console.log('============Large landscape-primary===================');
+      }  else if (widthWindow >= 768 && widthWindow < 992){
+        console.log('============Medium landscape-primary================');
+      } else if (widthWindow >= 576 && widthWindow < 767){
+        console.log("==============Small landscape-primary===============");
+      } else if (widthWindow < 576){
+        console.log('=============X-Small landscape-primary=============');
+      }
+    } else {
+      if (widthWindow >= 1200){
+        console.log("========= Extra Large portret-secondary===============");
+      }  else if(widthWindow >= 993 && widthWindow < 1199){
+        console.log('============Large portret-secondary===================');
+      }  else if (widthWindow >= 768 && widthWindow < 992){
+        console.log('============Medium portret-secondary================');
+      } else if (widthWindow >= 576 && widthWindow < 767){
+        console.log("==============Small portret-secondary===============");
+      } else if (widthWindow < 576){
+        console.log('=============X-Small portret-secondary=============');
+      }
     }
 };
 
-
+window.addEventListener("orientationchange", function makeResponsive(e));
 
