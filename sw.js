1769c81275dab296be1de71f90dd4e4f478818db
Natalia Agarkowa <agarkowa@gmail.com>
2018-07-06 18:24:19 +0200

    add service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-   let objectToCache = [
+   let arrayToCache = [
       '/',
       'index.html',
       'restaurant.html',
@@ -7,17 +7,16 @@
       '/js/dbhelper.js',
       '/js/restaurant_info.js',
       'data/restaurants.json',
-      '/img/1.jpg',
-      '/img/2.jpg',
-      '/img/3.jpg',
-      '/img/4.jpg',
-      '/img/5.jpg',
-      '/img/6.jpg',
-      '/img/7.jpg',
-      '/img/8.jpg',
-      '/img/9.jpg',
-      '/img/10.jpg'
-
+      'img/1.jpg',
+      'img/2.jpg',
+      'img/3.jpg',
+      'img/4.jpg',
+      'img/5.jpg',
+      'img/6.jpg',
+      'img/7.jpg',
+      'img/8.jpg',
+      'img/9.jpg',
+      'img/10.jpg'
    ];
 
   //Registration Service Worker
@@ -27,40 +26,38 @@
   }).catch(function(error) {
     console.log('Failed in registration', error);
   });
-} else {
-  console.log('Browsers API dont support service worker');
-}
-let CACHE = 'network-or-cache-v1';
-self.addEventListener('install', function(event){
-   event.waitUntil(
+  } else {
+    console.log('Browsers API dont support service worker');
+  }
+
+  //define cache name
+  let CACHE = 'network-or-cache-v1';
+  // add image to cache
+  self.addEventListener('install', function(event){
+    let offlinePage = new Request('/index.html');
+     self.skipWaiting();
+     event.waitUntil(
         caches
             .open(CACHE)
-            .then((cache) => cache.addAll(objectToCache))
+            .then((cache) => cache.addAll(arrayToCache))
       );
   });
 
-/*self.addEventListener('activate', function(event){
-  // Todo...
-});*/
-self.addEventListener('fetch', function(event) {
-      console.log(event.request.url);
-      //compare url with cache
-       if (event.request.url.endsWith('/restaurant.html'?id=2)) {
-      event.respondWith(
-        fetch('/restaurant.html')
-      );
-    } else event.respondWith(
-      fromCache(event.request));
-        event.waitUntil(
-            update(event.request));
+//get dates from cache, after that from Network
+self.addEventListener('fetch', function(event){
+  event.respondWith(fromCache(event.request));
+  event.waitUntil(update(event.request));
+});
 
-  });
+
 //get dates from cache
 function fromCache(request){
   return caches.open(CACHE)
-    .then(cache)=>(request)=>{caches.match(request)};
-    .then(matching)=>(reject)=>{matching || Promise.reject('no-match')};
-  };
+    .then((cache)=>cache.match((event.request.url, {ignoreSearch: true}))
+    .then((matching) || Promise.reject('no-match')
+    ));
+  }
+
 //update dates from network
 function update(request) {
   return caches.open(CACHE)
@@ -71,4 +68,10 @@ function update(request) {
       )
   );
 }
-
+//This is a event that can be fired from your page to tell the SW to update the offline page
+self.addEventListener('refreshOffline', function(response) {
+  return caches.open('CACHE').then(function(cache) {
+    console.log('[off] Offline page updated from refreshOffline event: '+ response.url);
+    return cache.put(offlinePage, response);
+  });
+});
